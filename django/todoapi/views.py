67b57911c7abe507fd5350d2830d55bd7da9d65d
nedialkom <nedialkom@gmail.com>
2020-04-19 17:11:08 +0300

    v1.13

--- django/todoapi/views.py
+++ django/todoapi/views.py
@@ -1,8 +1,10 @@
-from .models import Task
-from .serializers import TaskSerializer
+# For getting the username from the JWT token.
+from rest_framework_jwt.utils import jwt_decode_handler
+from .utils import get_auth0_user_id_from_request
 
-from rest_framework import generics
 
+
+# Lists and Creates entries of Task.
 class TaskList(generics.ListCreateAPIView):
     """
     Lists and creates tasks.
@@ -10,11 +12,17 @@ class TaskList(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
+    def perform_create(self, serializer):
+        auth0_user_id = get_auth0_user_id_from_request(self.request)
+        # Set the user to the one in the token.
+        serializer.save(created_by=auth0_user_id)
 
-class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
-    """
-    Returns a single Task and allows updates and deletion of a Task.
-    """
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
-    lookup_url_kwarg = 'task_id'
+    def get_queryset(self):
+        """
+        This view should return a list of all Tasks
+        for the currently authenticated user.
+        """
+        token = self.request.META.get('HTTP_AUTHORIZATION', '').split()[1]
+        payload = jwt_decode_handler(token)
+        auth0_user_id = payload.get('sub')
+        return Task.objects.filter(created_by=auth0_user_id)
