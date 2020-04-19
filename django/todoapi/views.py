5d9c1a1d9a79f628bdccbbc02081e2461921b496
nedialkom <nedialkom@gmail.com>
2020-04-19 18:18:04 +0300

    v1.14

--- django/todoapi/views.py
+++ django/todoapi/views.py
@@ -2,6 +2,12 @@
 from rest_framework_jwt.utils import jwt_decode_handler
 from .utils import get_auth0_user_id_from_request
 
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework import generics
+
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsCreator
 
 
 # Lists and Creates entries of Task.
@@ -26,3 +32,14 @@ class TaskList(generics.ListCreateAPIView):
         payload = jwt_decode_handler(token)
         auth0_user_id = payload.get('sub')
         return Task.objects.filter(created_by=auth0_user_id)
+
+# Return a single Task and allows its update and deletion.
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Returns a single Task and allows updates and deletion of a Task.
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated,IsCreator]
+    lookup_url_kwarg = 'task_id'
+
